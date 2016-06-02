5632fac785b3b71185e2b730c90fe5c44258b064
Arwa <arwaalalial@gmail.com>
2016-06-02 14:41:16 +0200

    app created

---
+++ dudas.js
@@ -0,0 +1,28 @@
+//var sample_array = [2, -5, 10, 5, 4, -10, 0 ]
+// function process(data){
+//  var positions = []; // quiero un array nuevo y lo inicializo como arrrat vacio 
+//  data.forEach (function (a, i) { // a y i pueden ser cualquier cosa, solamente se les nombra para decirles que comparar 
+//  data.forEach (function (b, j) {
+//  if (a + b === 0) {positions.push ( i +","+ j )}
+//  });
+//  });
+// }
+// process(sample_array); // cierra la funcion nombrando la funcion y la variable 
+// necesito comparar cada uno con todos y recorro todo mi array 
+// Se hace para realizar comparaciones, le estoy diciendo que me compare a con b a los numeros que estan dentro del array 
+//  no hay diferencia entre " " y ''
+//"My friend Izzy is \"special\"." // para que me imprima dentro de la " special" las comillas
+
+
+
+var numeros = [ 2, -2, 5, 6, 3, 0]
+function process (data){
+	var results = [];
+	data.forEach (function ( valor ))
+	data.forEach (function ( valor2)) {
+		if (valor + valor2 === 0 )
+	}
+	
+}
+process(numeros);
+
